263a1ed7c6e3585bf5419dfea3e4b5aeded8fb5c
Yntymask <abuyntymak@gmail.com>
2022-01-15 22:49:28 +0600

    Basic Car class is added

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,25 @@
 import UIKit
 
-var greeting = "Hello, playground"
-print("Hello, world")
+enum TrunkState {
+    case open, closed
+}
+
+enum EngineState {
+    case on, off
+}
+class Car {
+    var color: UIColor
+    var trunkState: TrunkState
+    var engineState: EngineState
+    
+    init(color: UIColor, trunkState: TrunkState, engineState: EngineState) {
+        self.color = color
+        self.trunkState = trunkState
+        self.engineState = engineState
+    }
+}
+
+let honda = Car(color: .black, trunkState: .open, engineState: .off)
+print(honda)
+
 
