e9a1ac2820a568ee222a8d91d0a90eaa6285b4b5
Yntymask <abuyntymak@gmail.com>
2022-01-17 19:52:51 +0600

    Lesson 4 howme work assignment

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -13,7 +13,8 @@ enum TrunkOccupation: String {
     case loadHalf = "Half of the trunk is loaded"
     case loadFull = "Trunk is full"
 }
-struct SportCar {
+
+class SportCar {
     let brand: String
     let productionYear: Int
     let trunkCapacity: Int
@@ -22,36 +23,80 @@ struct SportCar {
     let occupiedTrunkSpace: Double
     var trunkDescription: TrunkOccupation
     
-    mutating func engineControl() {
+    init(brand: String, productionYear: Int, trunkCapacity: Int, engineState: EngineState, windowState: WindowsState, occupiedTrunkSpace: Double, trunkDescription: TrunkOccupation) {
+        self.brand = brand
+        self.productionYear = productionYear
+        self.trunkCapacity = trunkCapacity
+        self.engineState = engineState
+        self.windowState = windowState
+        self.occupiedTrunkSpace = occupiedTrunkSpace
+        self.trunkDescription = trunkDescription
+    }
+    
+    func engineControl() {
         self.engineState = .on
         self.windowState = .closed
         self.trunkDescription = .loadHalf
     }
+    
+    func printProperties(){
+        print("Car brand: \(brand) \n Car production year is \(productionYear) \n Car's engine state: \(engineState) \n Car's window state: \(windowState)")
+    }
+     
 }
 
-struct TrunkCar {
+class TrunkCar {
     let brand: String
     let productionYear: Int
     let trunkCapacity: Int
     var engineState: EngineState
     var windowState: WindowsState
-    let occupiedTrunkSpace: Double
+    var occupiedTrunkSpace: Double
     var trunkDescription: TrunkOccupation
     
-    mutating func engineControl() {
+    
+    func engineControl() {
         self.engineState = .on
         self.windowState = .closed
         self.trunkDescription = .loadHalf
     }
+    
+    func loadTrunk (load: Double) {
+        let availableTrunkSpace = Double(trunkCapacity) - occupiedTrunkSpace
+        if  availableTrunkSpace >= load   {
+            occupiedTrunkSpace += load
+            print(occupiedTrunkSpace)
+        } else {
+            print("Only \(availableTrunkSpace) space is available in the trunk")
+        }
+    }
+    
+    init(brand: String, productionYear: Int, trunkCapacity: Int, engineState: EngineState, windowState: WindowsState, occupiedTrunkSpace: Double, trunkDescription: TrunkOccupation) {
+        self.brand = brand
+        self.productionYear = productionYear
+        self.trunkCapacity = trunkCapacity
+        self.engineState = engineState
+        self.windowState = windowState
+        self.occupiedTrunkSpace = occupiedTrunkSpace
+        self.trunkDescription = trunkDescription
+    }
+ 
 }
  
 
-var car1 = SportCar(brand: "BMW", productionYear: 2021, trunkCapacity: 1000, engineState: .off, windowState: .closed, occupiedTrunkSpace: 500, trunkDescription: .loadHalf)
-print(car1)
-
+        
+var car1 = SportCar(brand: "BMW", productionYear: 2021, trunkCapacity: 100, engineState: .off, windowState: .closed, occupiedTrunkSpace: 10, trunkDescription: .loadQuarter)
 car1.engineControl()
-print(car1)
+car1.printProperties()
 
-var trunk1 = TrunkCar(brand: "Mann", productionYear: 2020, trunkCapacity: 10000, engineState: .off, windowState: .open, occupiedTrunkSpace: 100, trunkDescription: .loadQuarter)
+var trunkCar = TrunkCar(brand: "MAN", productionYear: 2010, trunkCapacity: 25000, engineState: .on, windowState: .closed, occupiedTrunkSpace: 15000, trunkDescription: .loadHalf)
+trunkCar.engineControl()
+trunkCar.engineState
 
+trunkCar.engineState = .off
+
+trunkCar.engineControl()
+trunkCar.engineState
+
+trunkCar.loadTrunk(load: 14500)
 
